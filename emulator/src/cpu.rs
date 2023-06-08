91e18f41da15aaca35d4117b5a8b93c9fe9d438a
aidenzzz <yosefaiden@gmail.com>
2023-06-08 14:57:05 -0400

    cpu stuff

--- emulator/src/cpu.rs
+++ emulator/src/cpu.rs
@@ -1,83 +1,101 @@
-const ZERO_FLAG: u8 = 0b1000_0000;
-const SUBTRACT_FLAG: u8 = 0b0100_0000;
-const HALF_CARRY_FLAG: u8 = 0b0010_0000;
-const CARRY_FLAG: u8 = 0b0001_0000;
+//
+struct Cpu {
+    registers: Registers,
+}
 
-/*
-    Struct representing the CPU's registers.
-*/
 struct Registers {
-    a: u8, // Accumulator
-    f: FlagRegister, // Flags
-    b: u8, // B
-    c: u8, // C
-    d: u8, // D
-    e: u8, // E
-    h: u8, // H
-    l: u8, // L
+    a: u8,
+    f: u8,
+    b: u8,
+    c: u8,
+    d: u8,
+    e: u8,
+    h: u8,
+    l: u8
 }
 
-/*
-    Struct representing specifically the CPU's flag register.
-*/
-struct FlagRegister {
-    z: bool,
-    n: bool,
-    h: bool,
-    c: bool,
-}
-
-/*
-    Implemnts getters and setters for the CPU's registers.
-*/
 impl Registers {
+    fn get_af(&self) -> u16 {
+        return ((self.a as u16) << 8) | self.f as u16;
+    }
+
     fn get_bc(&self) -> u16 {
-        (self.b as u16) << 8 | (self.c as u16)
+        return ((self.b as u16) << 8) | self.c as u16;
     }
 
     fn get_de(&self) -> u16 {
-        (self.d as u16) << 8 | (self.e as u16)
+        return ((self.d as u16) << 8) | self.e as u16;
     }
 
     fn get_hl(&self) -> u16 {
-        (self.h as u16) << 8 | (self.l as u16)
+        return ((self.h as u16) << 8) | self.l as u16;
+    }
+
+    fn set_af(&mut self, value: u16) {
+        self.a = (value >> 8) as u8;
+        self.f = (value & 0xFF) as u8;
     }
 
     fn set_bc(&mut self, value: u16) {
-        self.b = ((value & 0xFF00) >> 8) as u8;
-        self.c = (value & 0x00FF) as u8;
+        self.b = (value >> 8) as u8;
+        self.c = (value & 0xFF) as u8;
     }
 
     fn set_de(&mut self, value: u16) {
-        self.d = ((value & 0xFF00) >> 8) as u8;
-        self.e = (value & 0x00FF) as u8;
+        self.d = (value >> 8) as u8;
+        self.e = (value & 0xFF) as u8;
     }
 
     fn set_hl(&mut self, value: u16) {
-        self.h = ((value & 0xFF00) >> 8) as u8;
-        self.l = (value & 0x00FF) as u8;
+        self.h = (value >> 8) as u8;
+        self.l = (value & 0xFF) as u8;
     }
-}
 
-impl std::convert::From<FlagRegister> for u8 {
-    fn from(flag: FlagRegister) -> u8 {
-        // this could be optimized i think? 
-        let mut result = 0;
-        if flag.z { result |= ZERO_FLAG; }
-        if flag.n { result |= SUBTRACT_FLAG; }
-        if flag.h { result |= HALF_CARRY_FLAG; }
-        if flag.c { result |= CARRY_FLAG; }
-        result
+    fn get_flag_z(&self) -> bool {
+        return (self.f & 0x80) != 0;
     }
-}
 
-impl std::convert::From<u8> for FlagRegister {
-    fn from(byte: u8) -> FlagRegister {
-        FlagRegister {
-            z: byte & ZERO_FLAG != 0,
-            n: byte & SUBTRACT_FLAG != 0,
-            h: byte & HALF_CARRY_FLAG != 0,
-            c: byte & CARRY_FLAG != 0,
+    fn get_flag_n(&self) -> bool {
+        return (self.f & 0x40) != 0;
+    }
+
+    fn get_flag_h(&self) -> bool {
+        return (self.f & 0x20) != 0;
+    }
+
+    fn get_flag_c(&self) -> bool {
+        return (self.f & 0x10) != 0;
+    }
+
+    fn set_flag_z(&mut self, value: bool) {
+        if value {
+            self.f |= 0x80;
+        } else {
+            self.f &= 0x7F;
+        }
+    }
+
+    fn set_flag_n(&mut self, value: bool) {
+        if value {
+            self.f |= 0x40;
+        } else {
+            self.f &= 0xBF;
+        }
+    }
+
+    fn set_flag_h(&mut self, value: bool) {
+        if value {
+            self.f |= 0x20;
+        } else {
+            self.f &= 0xDF;
+        }
+    }
+
+    fn set_flag_c(&mut self, value: bool) {
+        if value {
+            self.f |= 0x10;
+        } else {
+            self.f &= 0xEF;
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,6 @@ use emulator;
 fn main() {
     println!("Hello, world!");
     println!("2 + 2 = {}", emulator::add(2, 2));
+    let test: u16 = 0x80;
+    println!("test: {:02x}", test as u16);
 }
