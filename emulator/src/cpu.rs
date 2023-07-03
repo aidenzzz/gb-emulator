ff82beac5505459543bbdab437415827fce35faf
Aiden Turner <yosefaiden@gmail.com>
2023-07-03 00:16:18 -0400

    refactor

--- emulator/src/cpu.rs
+++ emulator/src/cpu.rs
@@ -54,26 +54,6 @@ impl CPU {
     fn execute_instruction(&mut self, opcode: u8) {
         match opcode {
             0x00 => self.inst_nop(),
-            0x01 => self.inst_ld_bc_nn(),
-            0x02 => self.inst_ld_bc_a(),
-            0x03 => self.inst_inc_bc(),
-            0x04 => self.inst_inc_b(),
-            0x05 => self.inst_dec_b(),
-            0x06 => self.inst_ld_b_n(),
-            0x07 => self.inst_rlca(),
-            0x08 => self.inst_ld_a16_sp(),
-            0x09 => self.inst_add_hl_bc(),
-            0x0A => self.inst_ld_a_bc(),
-            0x0B => self.inst_dec_bc(),
-            0x0C => self.inst_inc_c(),
-            0x0D => self.inst_dec_c(),
-            0x0E => self.inst_ld_c_n(),
-            0x0F => self.inst_rrca(),
-            0x10 => self.inst_stop_n(),
-            0x11 => self.inst_ld_de_nn(),
-            0x12 => self.inst_ld_de_a(),
-            0x13 => self.inst_inc_de(),
-            0x14 => self.inst_inc_d(),
             _ => panic!("Unsupported opcode: 0x{}", opcode),
         }
     }
@@ -88,145 +68,90 @@ impl CPU {
         // do nothing
     }
 
-    fn inst_ld_bc_nn(&mut self) {
-        let imm_data = self.read_next_byte();
-        self.reg_b = imm_data;
-        let imm_data = self.read_next_byte();
-        self.reg_c = imm_data;
-    }
+    fn add(&mut self, val: u8) {
+		let (res, overflow) = self.reg_a.overflowing_add(val);
+		self.reg_a = res;
+		self.fl_zero = self.reg_a == 0;
+		self.fl_sub = false;
+		self.fl_hc = (self.reg_a & 0x0F) + (val & 0x0F) > 0x0F;
+		self.fl_c = overflow;
+	}
 
-    fn inst_ld_bc_a(&mut self) {
-        let address = (self.reg_b as u16) << 8 | self.reg_c as u16;
-        self.mem_bus[address as usize] = self.reg_a;
-    }
+	fn adc(&mut self, val: u8) {
+		let (res, overflow) = self.reg_a.overflowing_add(val);
+		let carry = if self.fl_c { 1 } else { 0 };
+		let (res, overflow2) = res.overflowing_add(carry);
+		self.reg_a = res;
+		self.fl_zero = self.reg_a == 0;
+		self.fl_sub = false;
+		self.fl_hc = (self.reg_a & 0x0F) + (val & 0x0F) + carry > 0x0F;
+		self.fl_c = overflow || overflow2;
+	}
 
-    fn inst_inc_bc(&mut self) {
-        let bc_val = (self.reg_b as u16) << 8 | self.reg_c as u16;
-        let incremented_val = bc_val.wrapping_add(1);
-        self.reg_b = (incremented_val << 8) as u8;
-        self.reg_c = incremented_val as u8;
-    }
+	fn and(&mut self, val: u8) {
+		self.reg_a &= val;
+		self.fl_zero = self.reg_a == 0;
+		self.fl_sub = false;
+		self.fl_hc = true;
+		self.fl_c = false;
+	}
 
-    fn inst_inc_b(&mut self) {
-        self.reg_b = self.reg_b.wrapping_add(1);
-        self.fl_zero = self.reg_b == 0;
-        self.fl_sub = false;
-        self.fl_hc = (self.reg_b & 0x0F) == 0x00;
-    }
+	fn cp(&mut self, val: u8) {
+		self.fl_zero = self.reg_a == val;
+		self.fl_sub = true;
+		self.fl_hc = (self.reg_a & 0x0F) < (val & 0x0F);
+		self.fl_c = self.reg_a < val;
+	}
 
-    fn inst_dec_b(&mut self) {
-        self.reg_b = self.reg_b.wrapping_sub(1);
-        self.fl_zero = self.reg_b == 0;
-        self.fl_sub = true;
-        self.fl_hc = (self.reg_b & 0x0F) == 0x0F;
-    }
+	fn dec(&mut self, val: u8) -> u8 {
+		let res = val.wrapping_sub(1);
+		self.fl_zero = res == 0;
+		self.fl_sub = true;
+		self.fl_hc = (res & 0x0F) == 0x0F;
+		res
+	}
 
-    fn inst_ld_b_n(&mut self) {
-        let imm_data = self.read_next_byte();
-        self.reg_b = imm_data;
-    }
+	fn inc(&mut self, val: u8) -> u8 {
+		let res = val.wrapping_add(1);
+		self.fl_zero = res == 0;
+		self.fl_sub = false;
+		self.fl_hc = (res & 0x0F) == 0;
+		res
+	}
 
-    fn inst_rlca(&mut self) {
-        let msb = (self.reg_a & 0x80) >> 7;
-        self.fl_c = msb != 0;
-        self.reg_a = (self.reg_a << 1) | msb;
-        self.fl_zero = self.reg_a == 0;
-        self.fl_sub = false;
-        self.fl_hc = false;
-    }
+	fn or(&mut self, val: u8) {
+		self.reg_a |= val;
+		self.fl_zero = self.reg_a == 0;
+		self.fl_sub = false;
+		self.fl_hc = false;
+		self.fl_c = false;
+	}
 
-    fn inst_ld_a16_sp(&mut self) {
-        let imm_data = self.read_next_byte();
-        let imm_data2 = self.read_next_byte();
-        let address = (imm_data as u16) << 8 | imm_data2 as u16;
-        let address2 = address + 1;
-        self.mem_bus[address as usize] = self.sreg_sp as u8;
-        self.mem_bus[address2 as usize] = (self.sreg_sp >> 8) as u8;
-    }
+	fn sbc(&mut self, val: u8) {
+		let (res, overflow) = self.reg_a.overflowing_sub(val);
+		let carry = if self.fl_c { 1 } else { 0 };
+		let (res, overflow2) = res.overflowing_sub(carry);
+		self.reg_a = res;
+		self.fl_zero = self.reg_a == 0;
+		self.fl_sub = true;
+		self.fl_hc = (self.reg_a & 0x0F) < (val & 0x0F) + carry;
+		self.fl_c = overflow || overflow2;
+	}
 
-    fn inst_add_hl_bc(&mut self) {
-        let bc_val = (self.reg_b as u16) << 8 | self.reg_c as u16;
-        let hl_val = (self.reg_h as u16) << 8 | self.reg_l as u16;
+	fn sub(&mut self, val: u8) {
+		let (res, overflow) = self.reg_a.overflowing_sub(val);
+		self.reg_a = res;
+		self.fl_zero = self.reg_a == 0;
+		self.fl_sub = true;
+		self.fl_hc = (self.reg_a & 0x0F) < (val & 0x0F);
+		self.fl_c = overflow;
+	}
 
-        let new_value = hl_val.wrapping_add(bc_val);
-        self.fl_hc = ((hl_val & 0xFFF) + (bc_val & 0xFFF)) > 0xFFF;
-        self.fl_c = new_value < hl_val;
-        self.fl_sub = false;
-
-        self.reg_h = (new_value << 8) as u8;
-        self.reg_l = new_value as u8;
-    }
-
-    fn inst_ld_a_bc(&mut self) {
-        let address = (self.reg_b as u16) << 8 | self.reg_c as u16;
-        self.reg_a = self.mem_bus[address as usize];
-    }
-
-    fn inst_dec_bc(&mut self) {
-        let bc_val = (self.reg_b as u16) << 8 | self.reg_c as u16;
-        let decremented_val = bc_val.wrapping_sub(1);
-        self.reg_b = (decremented_val << 8) as u8;
-        self.reg_c = decremented_val as u8;
-    }
-
-    fn inst_inc_c(&mut self) {
-        self.reg_c = self.reg_c.wrapping_add(1);
-        self.fl_zero = self.reg_c == 0;
-        self.fl_sub = false;
-        self.fl_hc = (self.reg_c & 0x0F) == 0x00;
-    }
-
-    fn inst_dec_c(&mut self) {
-        self.reg_c = self.reg_c.wrapping_sub(1);
-        self.fl_zero = self.reg_c == 0;
-        self.fl_sub = true;
-        self.fl_hc = (self.reg_c & 0x0F) == 0x0F;
-    }
-
-    fn inst_ld_c_n(&mut self) {
-        let imm_data = self.read_next_byte();
-        self.reg_c = imm_data;
-    }
-
-    fn inst_rrca(&mut self) {
-        let lsb = self.reg_a & 0x01;
-        self.fl_c = lsb != 0;
-        self.reg_a = (self.reg_a >> 1) | (lsb << 7);
-
-        self.fl_zero = self.reg_a == 0;
-        self.fl_sub = false;
-        self.fl_hc = false;
-    }
-
-    fn inst_stop_n(&mut self) {
-        // sets the cpu to low power mode. not implemented
-        // reads the next byte, but does nothing with it
-        let _ = self.read_next_byte();
-    }
-
-    fn inst_ld_de_nn(&mut self) {
-        let imm_data = self.read_next_byte();
-        self.reg_d = imm_data;
-        let imm_data = self.read_next_byte();
-        self.reg_e = imm_data;
-    }
-
-    fn inst_ld_de_a(&mut self) {
-        let address = (self.reg_d as u16) << 8 | self.reg_e as u16;
-        self.mem_bus[address as usize] = self.reg_a;
-    }
-
-    fn inst_inc_de(&mut self) {
-        let de_val = (self.reg_d as u16) << 8 | self.reg_e as u16;
-        let incremented_val = de_val.wrapping_add(1);
-        self.reg_d = (incremented_val << 8) as u8;
-        self.reg_e = incremented_val as u8;
-    }
-
-    fn inst_inc_d(&mut self) {
-        self.reg_d = self.reg_d.wrapping_add(1);
-        self.fl_zero = self.reg_d == 0;
-        self.fl_sub = false;
-        self.fl_hc = (self.reg_d & 0x0F) == 0x00;
-    }
+	fn xor(&mut self, val: u8) {
+		self.reg_a ^= val;
+		self.fl_zero = self.reg_a == 0;
+		self.fl_sub = false;
+		self.fl_hc = false;
+		self.fl_c = false;
+	}
 }
